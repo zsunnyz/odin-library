039354143f81f173865b33fc039690a5dbe29373
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-23 20:11:44 +1000

    added add book button functionality to show modal

--- library.js
+++ library.js
@@ -1,1 +1,15 @@
-addBtn = document.getElementById("newBook");
+/* add book button elements */
+const addBtn = document.getElementById("new-book");
+const plusSVG = document.getElementById("plus");
+
+/* modal elements */
+const modal = document.querySelector(".modal");
+const modalEdit = document.getElementById("modal-edit");
+const modalAdd = document.getElementById("modal-add");
+
+addBtn.addEventListener("click", () => {
+    plusSVG.classList.toggle("plus-close");
+    modal.classList.toggle("noshow");
+    modalEdit.classList.add("noshow");
+    modalAdd.classList.remove("noshow");
+})
