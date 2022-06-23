b5bd45387a1044d8b3db278c4d75856c16cd6b8d
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-23 20:32:59 +1000

    added book object

--- library.js
+++ library.js
@@ -1,15 +1,44 @@
+/* Library Objects */
+let currLibrary = [];
+
+function Book() {
+    this.title = document.getElementById("title").value;
+    this.author = document.getElementById("author").value;
+    this.pages = document.getElementById("total-pages").value;
+    this.pagesRead = document.getElementById("pages-read").value;
+    
+    this.isRead = () => (this.pages == this.pagesRead);
+}
+
+function addBookToLibrary() {
+    
+}
+
 /* add book button elements */
 const addBtn = document.getElementById("new-book");
 const plusSVG = document.getElementById("plus");
 
 /* modal elements */
 const modal = document.querySelector(".modal");
-const modalEdit = document.getElementById("modal-edit");
-const modalAdd = document.getElementById("modal-add");
+const modalContent = document.querySelector(".modal-content");
+
+const modalSubmit = document.getElementById("modal-submit");
+
+function toggleBookAdd() {
+    document.querySelector(".modal-content > h2").value = "Add New Book";
+    modal.classList.toggle("noshow");
+}
+
+function toggleBookEdit() {
+    document.querySelector(".modal-content > h2").value = "Edit Book";
+    modal.classList.toggle("noshow");
+}
 
 addBtn.addEventListener("click", () => {
     plusSVG.classList.toggle("plus-close");
-    modal.classList.toggle("noshow");
-    modalEdit.classList.add("noshow");
-    modalAdd.classList.remove("noshow");
+    toggleBookAdd();
+})
+
+modalSubmit.addEventListener("click", () => {
+
 })
