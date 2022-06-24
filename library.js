3bf549a680d7b86a088ac10f865292098f08539a
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-24 20:14:00 +1000

    added input validation for total pages and pages read. Created an object to track all the inputs validity

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+let isNumber = num => {return /^\d+$/.test(num);}
+
 /* Library Objects */
 let currLibrary = [];
 
@@ -22,7 +24,88 @@ const plusSVG = document.getElementById("plus");
 const modal = document.querySelector(".modal");
 const modalContent = document.querySelector(".modal-content");
 
+// input validation
+allInputsValid = {} // object containing all the input id as keys and if they are valid or not
+
+function areInputsValid() {
+    Object.values(allInputsValid).forEach(value => {
+        if(!value){
+            return false;
+        }
+    })
+    return true;
+}
+
+const modalInputs = document.querySelectorAll(".modal-content > input");
+modalInputs.forEach(modalInput => {
+    allInputsValid[modalInput.id] = false;
+});
+
+/* input event listener events */
+// change the value of pages read based on total-pages if book is finished
+["keyup", "keydown"].forEach(e => {
+    document.getElementById("total-pages").addEventListener(e , () => {
+        if (document.getElementById("pages-read").readOnly) {
+            document.getElementById("pages-read").value = document.getElementById("total-pages").value;
+        }
+
+        pageTotalSmall = document.getElementById("page-total-small-e");
+        pageTotalAlpha = document.getElementById("page-total-alpha-e");
+
+        if (!isNumber(document.getElementById("total-pages").value) && document.getElementById("total-pages").value != ""){
+            console.log("here");
+            pageTotalSmall.classList.add("noshow");
+            pageTotalAlpha.classList.remove("noshow");
+        }
+        else if (document.getElementById("total-pages").value <= '0') {
+            pageTotalAlpha.classList.add("noshow");
+            pageTotalSmall.classList.remove("noshow");
+        }
+        else {
+            pageTotalAlpha.classList.add("noshow");
+            pageTotalSmall.classList.add("noshow");
+            allInputsValid["total-pages"] = true;
+        }
+    })
+});
+
+// event listener for the book finished button in the modal event
+let currInputPage = 0;
+const modalCompleted = document.getElementById("completed");
+modalCompleted.addEventListener("click", () => {
+    if (modalCompleted.checked) {
+        pagesReadInput = document.getElementById("pages-read");
+
+        currInputPage = pagesReadInput.value;
+        pagesReadInput.value = document.getElementById("total-pages").value;
+        pagesReadInput.readOnly = true;
+    }
+    else {
+        pagesReadInput.readOnly = false;
+        pagesReadInput.value = currInputPage;
+    }
+});
+
 const modalSubmit = document.getElementById("modal-submit");
+// Looping through all the modal inputs to make them not invalid on startup
+modalInputs.forEach(modalInput => {
+    modalInput.addEventListener("keyup", e => {
+        if (e.target.value.length > 0) {
+            e.target.classList.remove("unfilled");
+        }
+    })
+});
+
+function isModalOpen(){
+    return modal.classList("noshow");
+}
+
+// Open modal button functions 
+addBtn.addEventListener("click", () => {
+    currInputPage = 0;
+    plusSVG.classList.toggle("plus-close");
+    toggleBookAdd();
+})
 
 function toggleBookAdd() {
     document.querySelector(".modal-content > h2").value = "Add New Book";
@@ -34,10 +117,6 @@ function toggleBookEdit() {
     modal.classList.toggle("noshow");
 }
 
-addBtn.addEventListener("click", () => {
-    plusSVG.classList.toggle("plus-close");
-    toggleBookAdd();
-})
 
 modalSubmit.addEventListener("click", () => {
 
