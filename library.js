71d85a46ba7f0ea38e892a614cc978ed49091b42
Sunny Zhang <zhasunny0001@gmail.com>
2022-06-29 02:34:14 +1000

    added function to add a card to page based on modal inputs

--- library.js
+++ library.js
@@ -1,25 +1,9 @@
-let isNumber = num => {return /^\d+$/.test(num);}
-
-/* Library Objects */
-let currLibrary = [];
-
-function Book() {
-    this.title = document.getElementById("title").value;
-    this.author = document.getElementById("author").value;
-    this.pages = document.getElementById("total-pages").value;
-    this.pagesRead = document.getElementById("pages-read").value;
-    
-    this.isRead = () => (this.pages == this.pagesRead);
-}
-
-function addBookToLibrary() {
-    
-}
-
 /* add book button elements */
 const addBtn = document.getElementById("new-book");
 const plusSVG = document.getElementById("plus");
 
+const contentContainer = document.getElementById("content-container");
+const contentTemplate = document.getElementById("card-template");
 /* modal elements */
 const modal = document.querySelector(".modal");
 const modalContent = document.querySelector(".modal-content");
@@ -31,7 +15,86 @@ const totalPagesInput = document.getElementById("total-pages");
 const pagesReadInput = document.getElementById("pages-read");
 const modalInputs = document.querySelectorAll(".modal-content > input");
 
+const modalCompleted = document.getElementById("completed");
+const modalSubmit = document.getElementById("modal-submit");
+
+let isNumber = num => {return /^\d+$/.test(num);}
+
+/* Library Objects */
+let currLibrary = [];
+
+function Book(id, title, author, pages, pagesRead, read) {
+    this.id = id;
+    this.title = title; 
+    this.author = author; 
+    this.pages = pages; 
+    this.pagesRead = pagesRead; 
+    this.read = read;
+    
+    this.isRead = () => (this.pages == this.pagesRead);
+}
+
+function addBookToLibrary() {
+    let newId = 0;
+    if (currLibrary.length > 0) {
+        newId = currLibrary[currLibrary.length - 1].id + 1;
+    }
+    currLibrary.push(new Book(newId, titleInput.value, authorInput.value, totalPagesInput.value, pagesReadInput.value, (totalPagesInput.value == pagesReadInput.value)));
+    addCardToPage(newId);
+}
+
+function getBookById(bookId) {
+    for (let book of currLibrary){
+        if (book.id == bookId) {
+            return book;
+        }
+    }
+
+    console.log(`No book with ID ${bookId} found!`);
+    return NaN;
+}
+
+function addCardToPage(bookId){
+
+    let newBook = getBookById(bookId);
+    let newCard = contentTemplate.content.cloneNode(true);
+
+    console.log(newBook);
+
+    newCard.querySelector(".card").id = `card-${newBook.id}`;
+    newCard.querySelector(".title").textContent = newBook.title;
+    newCard.querySelector(".author").textContent = newBook.author;
+    newCard.querySelector(".total-pages").textContent = newBook.pages;
+    newCard.querySelector(".pages-read").textContent = newBook.pagesRead;
+    
+    newCard.querySelector("input").id = `input-${newBook.id}`;
+    newCard.querySelector("input").value = newBook.pagesRead;
+    newCard.querySelector("input").max = newBook.pages;
+    
+    newCard.querySelectorAll("button").forEach(button => {
+        button.setAttribute("data-id", newBook.id);
+    }) 
+
+    if (newBook.read) {
+        newCard.querySelector(".card").classList.add("read");
+    }
+    else {
+        newCard.querySelector(".card").classList.add("unfinished");
+    }
+        
+    contentContainer.appendChild(newCard);
+}
+
+function resetModal() {
+    modalInputs.forEach(modalInput => {
+        modalInput.value = "";
+        modalInput.classList.add("unfilled");
+    })
+    modalSubmit.classList.add("inactive");
+}
+
 let allInputsValid = {} // object containing all the input id as keys and if they are valid or not
+
 modalInputs.forEach(modalInput => {
     allInputsValid[modalInput.id] = false;
 });
@@ -47,7 +110,6 @@ function areInputsValid() {
 };
 
 function changeOnInputsValid() {
-    console.log(areInputsValid());
     if (areInputsValid()){
         modalSubmit.classList.remove("inactive");
     }
@@ -55,8 +117,17 @@ function changeOnInputsValid() {
         modalSubmit.classList.add("inactive");
     }
 }
-/* input event listener events */
 
+// Looping through all the modal inputs to make them not invalid on startup
+modalInputs.forEach(modalInput => {
+    modalInput.addEventListener("keyup", e => {
+        if (e.target.value.length > 0) {
+            e.target.classList.remove("unfilled");
+        }
+    })
+});
+
+/* input event listeners*/
 titleInput.addEventListener("keydown", () => {
     if (titleInput.value.length >= 1){
         allInputsValid["title"] = true;
@@ -127,9 +198,8 @@ function checkPagesReadValid(){
     changeOnInputsValid();
 }
 
-// event listener for the book finished button in the modal event
+// event listener for the book finished checkbox in the modal event
 let currInputPage = 0;
-const modalCompleted = document.getElementById("completed");
 modalCompleted.addEventListener("click", () => {
     if (modalCompleted.checked) {
 
@@ -144,15 +214,12 @@ modalCompleted.addEventListener("click", () => {
     }
 });
 
-const modalSubmit = document.getElementById("modal-submit");
-// Looping through all the modal inputs to make them not invalid on startup
-modalInputs.forEach(modalInput => {
-    modalInput.addEventListener("keyup", e => {
-        if (e.target.value.length > 0) {
-            e.target.classList.remove("unfilled");
-        }
-    })
-});
+modalSubmit.addEventListener("click", () => {
+    if (document.querySelector(".modal-content > h2").value == "Add New Book" && areInputsValid()) {
+        addBookToLibrary();
+        resetModal();
+    }
+})
 
 function isModalOpen(){
     return modal.classList("noshow");
@@ -175,7 +242,3 @@ function toggleBookEdit() {
     modal.classList.toggle("noshow");
 }
 
-
-modalSubmit.addEventListener("click", () => {
-
-})
